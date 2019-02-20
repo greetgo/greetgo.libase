158beb7d3c77ff3a218f37a5922009ff332a172e
ekolpakov <ekolpakov@greet-go.com>
2019-02-20 15:01:57 +0600

    Настроено корректное подключение драйверов оракла

--- test_src/kz/greetgo/libase/utils/DbWorkerOracle.java
+++ test_src/kz/greetgo/libase/utils/DbWorkerOracle.java
@@ -3,6 +3,7 @@ package kz.greetgo.libase.utils;
 import kz.greetgo.conf.sys_params.SysParams;
 import kz.greetgo.libase.errors.IllegalOracleVersion;
 import kz.greetgo.libase.errors.NoDbObject;
+import org.testng.SkipException;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -29,26 +30,36 @@ public class DbWorkerOracle extends AbstractDbWorker {
 
   @Override
   public Connection connection(DbSide dbSide) throws Exception {
-    Class.forName("oracle.jdbc.driver.OracleDriver");
+    loadOracleDriver();
     ConnectParams params = connectParams(dbSide);
     return DriverManager.getConnection(params.url(), params.username(), params.password());
   }
 
+  public static void loadOracleDriver() {
+    try {
+      Class.forName("oracle.jdbc.driver.OracleDriver");
+    } catch (ClassNotFoundException e) {
+      throw new SkipException("ClassNotFound : oracle.jdbc.driver.OracleDriver");
+    }
+  }
+
   @Override
   public void recreateDb(DbSide dbSide) throws Exception {
-    Class.forName("oracle.jdbc.driver.OracleDriver");
+    loadOracleDriver();
 
     try (Connection con = getAdminConnection()) {
 
       try {
         exec(con, "alter session set \"_ORACLE_SCRIPT\"=true");
-      } catch (IllegalOracleVersion ignore) {}
+      } catch (IllegalOracleVersion ignore) {
+      }
 
       ConnectParams params = connectParams(dbSide);
 
       try {
         exec(con, "drop user " + params.username() + " cascade");
-      } catch (NoDbObject ignore) {}
+      } catch (NoDbObject ignore) {
+      }
 
       exec(con, "create user " + params.username() + " identified by " + params.password());
       exec(con, "grant all privileges to " + params.username());
